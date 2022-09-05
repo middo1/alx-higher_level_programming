0e68053e94e2ab347b93f543afc2925a73507fb5
middo1 <hamidadeshina1@gmail.com>
2022-09-05 21:25:40 +0100

    fifth task

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Accepts two arguments from the terminal,
+concatenates the arguments in format
+arg1 is arg2
+and prints the result
+*/
+console.log(process.argv[2] + ' is ' + process.argv[3]);
