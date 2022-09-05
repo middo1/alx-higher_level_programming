d8cd512d8463d85d6eb6fe7638d3748b5e75f9a9
middo1 <hamidadeshina1@gmail.com>
2022-09-05 21:42:30 +0100

    not the same as my python

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+Print three strings from an array to the console
+*/
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < 3; x++) {
+  console.log(myArr[x]);
+}
